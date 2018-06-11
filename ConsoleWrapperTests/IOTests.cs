71fe8895b120ba7bd53f771ae25f459ad83d4f74
Carl <36981953+FalconEye36@users.noreply.github.com>
2018-06-11 21:30:56 +1200

    Fiddling with EchoConsole and Tests

--- ConsoleWrapperTests/IOTests.cs
+++ ConsoleWrapperTests/IOTests.cs
@@ -9,26 +9,22 @@ namespace ConsoleWrapperTests
         [Fact]
         public void TestInput()
         {
+            System.Diagnostics.Trace.Listeners.Add(new System.Diagnostics.TextWriterTraceListener(Console.Out));
             using (CWrapper wrapper = new CWrapper(Constants.ECHO_CONSOLE_LOCATION))
             {
                 const string DATA = "This data shall be echoed";
                 string echoData = String.Empty;
-                wrapper.OutputDataReceived += (s, e) => echoData = e.Data;
-                wrapper.OutputDataReceived += Stub;
+                //wrapper.OutputDataReceived += (s, e) => echoData = e.Data;
+                //wrapper.OutputDataReceived += (s, e) => Assert.Equal(DATA, e.Data);
 
-                wrapper.Execute();
+                wrapper.Execute(EchoConsole.Program.FLOOD_KEY);
 
-                wrapper.WriteToConsole(DATA);
-                DateTime startTime = DateTime.UtcNow;
+                //wrapper.WriteToConsole(DATA);
+                /*DateTime startTime = DateTime.UtcNow;
                 while (startTime.AddMilliseconds(10) > DateTime.UtcNow && echoData?.Length == 0)
                     continue;
-                Assert.Equal(DATA, echoData);
+                Assert.Equal(DATA, echoData);*/
             }
-        }
-
-        public void Stub(object sender, EventArgs e)
-        {
-            return;
         }
     }
 }
--- EchoConsole/Program.cs
+++ EchoConsole/Program.cs
@@ -4,8 +4,20 @@ namespace EchoConsole
 {
     public static class Program
     {
+        /// <summary>
+        /// Indicates the the console should throw an exception on start
+        /// </summary>
         public const string EXCEPTION_KEY = "throwException";
+
+        /// <summary>
+        /// Indicates the the console should exit on start
+        /// </summary>
         public const string EXIT_KEY = "exitOnStart";
+
+        /// <summary>
+        /// Indicates that the console should flood the output stream with the text 'ping'
+        /// </summary>
+        public const string FLOOD_KEY = "floodOnStart";
 
         public static void Main(string[] args)
         {
@@ -17,11 +29,18 @@ namespace EchoConsole
                         throw new Exception("This exception was requested by the caller");
                     case EXIT_KEY:
                         return;
+                    case FLOOD_KEY:
+                        while (true)
+                            Console.WriteLine("ping");
+                    default:
+                        while (true)
+                            Console.WriteLine(Console.ReadLine());
                 }
+            } else
+            {
+                while (true)
+                    Console.WriteLine(Console.ReadLine());
             }
-
-            while (true)
-                Console.WriteLine("Bob");
         }
     }
 }
